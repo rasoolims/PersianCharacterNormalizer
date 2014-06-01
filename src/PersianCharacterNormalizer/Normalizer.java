3ae20491e7767d2f412a64c6a3cd67cceea7d2f1
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2014-05-31 23:35:34 -0400

    added semi-space constraints

--- src/PersianCharacterNormalizer/Normalizer.java
+++ src/PersianCharacterNormalizer/Normalizer.java
@@ -1,10 +1,8 @@
 package PersianCharacterNormalizer;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Vector;
 
 /**
  * Created by Mohammad Sadegh Rasooli.
@@ -16,9 +14,12 @@ import java.util.HashMap;
 
 public class Normalizer {
     HashMap<String,String> charMap;
+    HashMap<String,String> illegalSemiSpaceMap;
+
 
     public Normalizer(String mapFile) throws IOException {
          charMap=new HashMap<String, String>();
+        illegalSemiSpaceMap=new HashMap<String, String>();
 
         BufferedReader reader=new BufferedReader(new FileReader(mapFile));
         String line="";
@@ -35,18 +36,54 @@ public class Normalizer {
         charMap.put("\u200D","\u200C") ;
         charMap.put("\u200F","\u200C") ;
         charMap.put("\u2029","\u200C") ;
+
+        String illegalSemiSpaces="\t ادذرزژوؤاآأإ";
+        String illegalNextChars="آاأإ" ;
+        for (int i=0;i<illegalSemiSpaces.length();i++){
+            String str=String.valueOf(illegalSemiSpaces.charAt(i));
+            String illegal=str+ "\u200C";
+            illegalSemiSpaceMap.put(illegal,str);
+        }
+        for (int i=0;i<illegalNextChars.length();i++){
+            String str=String.valueOf(illegalNextChars.charAt(i));
+            String illegal="\u200C"+str;
+            illegalSemiSpaceMap.put(illegal,str);
+        }
     }
 
     public String normalize(String text){
-        StringBuilder newText=new StringBuilder();
+        text=text.replace("\r","").replace("هٔ","ه‌ی").replace("...","…");
+
+       StringBuilder newText=new StringBuilder();
        for (int i=0;i<text.length();i++){
            String thisChar=String.valueOf(text.charAt(i));
            if(charMap.containsKey(thisChar))
                thisChar=charMap.get(thisChar);
            newText.append(thisChar);
-
        }
+        text= newText.toString();
 
-        return newText.toString();
+        for(String illegalSequence:illegalSemiSpaceMap.keySet()){
+            if(text.contains(illegalSequence))
+                text=text.replace(illegalSequence,illegalSemiSpaceMap.get(illegalSequence));
+        }
+
+        return text;
+    }
+
+    public void normalizeFile(String file,String outputFile) throws Exception{
+        BufferedReader reader=new BufferedReader(new FileReader(file));
+        BufferedWriter writer=new BufferedWriter(new FileWriter(outputFile));
+        int counter=0;
+        String line="";
+        while((line=reader.readLine())!=null){
+            writer.write(normalize(line.trim())+"\n");
+            counter++;
+            if(counter%1000000==0)
+                System.err.print(counter + "...");
+        }
+        writer.flush();
+        writer.close();
+        System.err.println("\nnormalize file done!");
     }
 }
--- src/Test/NormalizerTest.java
+++ src/Test/NormalizerTest.java
@@ -13,11 +13,24 @@ import java.io.IOException;
  */
 
 public class NormalizerTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //path to map file (use the file charmap.txt)
         String mapFile="/Users/msr/Projects/PersianTextNormalizer/src/Test/charmap.txt";
+
+
+        if (args.length>=1){
+            mapFile=args[0];
+        }
+
         Normalizer normalizer=new Normalizer(mapFile);
         System.out.println("مي‌شود 1 ۲ 3");
-        System.out.print(normalizer.normalize("مي‌شود 1 ۲ 3"));
+        System.out.println(normalizer.normalize("مي‌شود 1 ۲ 3"));
+
+        if(args.length>=3){
+            String inputFile=args[1];
+            String outputFile=args[2];
+
+           normalizer.normalizeFile(inputFile,outputFile);
+        }
     }
 }
